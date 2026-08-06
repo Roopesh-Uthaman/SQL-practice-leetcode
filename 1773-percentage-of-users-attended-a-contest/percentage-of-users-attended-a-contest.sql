f2926cdf8a1acffd3aae117d2236286ae3ebbd85
Roopesh US <78408388+Roopesh-Uthaman@users.noreply.github.com>
2026-08-06 11:55:15 +0530

    Time: 1066 ms (43.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,9 @@
+select contest_id,
+ROUND(
+        COUNT(user_id) * 100.0 /
+        (SELECT COUNT(*) FROM Users),
+        2
+    ) AS percentage
+from Register
+group by contest_id
+order by percentage desc, contest_id asc
