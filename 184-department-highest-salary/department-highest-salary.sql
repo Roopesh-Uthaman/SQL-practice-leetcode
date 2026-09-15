9f57168ba0d4072a374d0f471071c564d015b03e
Roopesh US <78408388+Roopesh-Uthaman@users.noreply.github.com>
2026-09-15 16:25:12 +0530

    Time: 654 ms (91.45%) | Memory: 0B (100.00%) - LeetSync

---
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,12 @@
+WITH RankedSalaries AS (
+    SELECT 
+        d.name AS Department, 
+        e.name AS Employee, 
+        e.salary AS Salary,
+        RANK() OVER (PARTITION BY e.departmentId ORDER BY e.salary DESC) AS rk
+    FROM Employee e
+    JOIN Department d ON e.departmentId = d.id
+)
+SELECT Department, Employee, Salary
+FROM RankedSalaries
+WHERE rk = 1;
