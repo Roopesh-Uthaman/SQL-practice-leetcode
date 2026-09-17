14270d3582660967474066c123637cf56023118c
Roopesh US <78408388+Roopesh-Uthaman@users.noreply.github.com>
2026-09-17 15:21:07 +0530

    Time: 784 ms (90.68%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,12 +1,13 @@
-SELECT person_name
-FROM (
+WITH RunningWeight AS (
     SELECT 
         person_name,
         weight,
         turn,
         SUM(weight) OVER (ORDER BY turn) AS total_weight
     FROM Queue
-) q
+)
+SELECT person_name 
+FROM RunningWeight
 WHERE total_weight <= 1000
 ORDER BY turn DESC
 LIMIT 1;
