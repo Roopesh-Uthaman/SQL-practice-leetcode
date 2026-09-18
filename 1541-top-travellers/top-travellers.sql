ab6c168bfde7058920573a2480831f734608e079
Roopesh US <78408388+Roopesh-Uthaman@users.noreply.github.com>
2026-09-18 11:28:07 +0530

    Time: 987 ms (44.74%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1541-top-travellers/top-travellers.sql
@@ -0,0 +1,5 @@
+select u.name, ifnull(sum(r.distance),0) as travelled_distance
+from rides r right join users u 
+on r.user_id = u.id
+group by r.user_id,u.name
+order by travelled_distance desc, name asc
